6167b0831481fbd2aef96a45f2e158b82b47e3de
Orlando Quero <orlaqp@gmail.com>
2010-09-03 14:48:31 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudBooks::Application.config.secret_token = '552f5bcd393a9461d4c8dcc943874cc60c57a0d92e3dd31af1ac6a59cc615e22d001de9e2fb07bbc74665fd3692412d42f454ad8ad7264a3e5d66e09fd9f89b3'
